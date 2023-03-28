535f7effe233c355fb0d73bbef288b630099d311
fisherreese2R <s540308@nwmissouri.edu>
2023-03-28 15:45:16 -0500

    Added more to sum then displayed answer

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+sum = 0
+more = 0
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more += 1
+  sum += more
+  res.send(`Sum is: ${sum}`);
 });
 
 module.exports = router;
